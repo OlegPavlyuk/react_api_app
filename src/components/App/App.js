337a2c4072f4632c508c6bff4ba41852f7bcb8f5
Oleg Pavlyuk <pavlyuk.o.yu@gmail.com>
2019-11-01 12:24:42 +0200

    add error boundaries

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,34 +1,30 @@
 import React, { Component } from 'react';
 import AppHeader from '../AppHeader';
 import RandomPlanet from '../RandomPlanet';
-import ItemList from '../ItemList';
-import PersonDetails from '../PersonDetails';
+import PeoplePage from '../PeoplePage';
+import ErrorIndicator from '../ErrorIndicator';
 
 import './App.css';
 
 export default class App extends Component {
   state = {
-    selectedPerson: null
+    hasError: false
   };
 
-  onPersonSelected = id => {
-    this.setState({ selectedPerson: id });
-  };
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
 
   render() {
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
+    }
+
     return (
       <div>
         <AppHeader />
         <RandomPlanet />
-
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onPersonSelected} />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson} />
-          </div>
-        </div>
+        <PeoplePage />
       </div>
     );
   }
